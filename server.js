a8407669eb57602853e84dc56d48d1423edf4df1
bluerface <fallingsilkworm@gmail.com>
2017-03-02 15:16:24 +0000

    [FEAT-1] Add serving of static files

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 var router = require('./routes');
@@ -6,6 +7,8 @@ var router = require('./routes');
 app.set('view engine', 'ejs');
 
 app.use('/', router);
+
+app.use(express.static(path.join(__dirname, '/static')));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
